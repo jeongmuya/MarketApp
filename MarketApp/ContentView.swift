563a9418f51c2c4896b6c18c05005a08617113b2
jeongmu <jeongmuya@gmail.com>
2025-12-08 00:36:05 +0900

    화면 + 데이터 가져오는 함수구현

--- MarketApp/ContentView.swift
+++ MarketApp/ContentView.swift
@@ -12,12 +12,37 @@ struct Product: Codable, Identifiable {
 }
 
 struct ContentView: View {
+    @State var products: [Product] = []
     var body: some View {
         NavigationStack {
-            Text("Hello, World!")
+            List(products) { product in
+                HStack {
+                    Text(product.title)
+                        .font(.headline)
+                    Text("$\(product.price)")
+                }
+            }
+        }
+        .navigationTitle(Text("중고마켓"))
+        .task {
+            await loadData()
         }
     }
-}
+    // 데이터 가져오는 함수
+        func loadData() async {
+            guard let url = URL(string: "https://dummyjson.com/products") else { return }
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let decodedResponse = try JSONDecoder().decode(ProductResponse.self, from: data)
+                self.products = decodedResponse.products
+            } catch {
+                print("에러 발생: \(error)")
+            }
+        }
+    }
+
+
 
 
 #Preview {
