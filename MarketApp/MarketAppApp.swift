982bb54f352e6777bda3912b044731c0048cdb7a
jeongmu <jeongmuya@gmail.com>
2025-12-07 22:35:30 +0900

    first commit

---
+++ MarketApp/MarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketAppApp.swift
+//  MarketApp
+//
+//  Created by 정무 on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
